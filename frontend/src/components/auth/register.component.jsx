643347a1c585fc10c025cd95d221f7a67f15a4e3
Naitik Rauniyar <64009612+naitik0009@users.noreply.github.com>
2022-10-29 04:52:21 +0545

    .
    .

--- frontend/src/components/auth/register.component.jsx
+++ frontend/src/components/auth/register.component.jsx
@@ -2,22 +2,39 @@ import { ActivityIndicator, StyleSheet, Text, TouchableOpacity, View } from "rea
 import { Button, TextInput } from "react-native"
 import { Formik } from "formik"
 import * as yup from "yup";
+import axios from "axios"
 import { validate } from "email-validator";
-import { authentication } from "../../services/data/databases/firebase.config";
-import {createUserWithEmailAndPassword} from "firebase/auth";
+import { authentication, db } from "../../services/data/databases/firebase.config";
+import { collection, getDocs, addDoc } from 'firebase/firestore/lite';
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import React from "react";
 
 export const RegisterComponent = ({ navigation }) => {
-    const [loading,setLoading] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
     const RegisterFormSchema = yup.object().shape({
         email: yup.string().required("An email is required"),
         username: yup.string().required("Enter a username").min(6, "Your username should be atleast 6 character long"),
         password: yup.string().required().min(8, "Your password should be atleast 8 character long")
     });
-    const SignUp= async(email,username,password) => {
+
+    const RandomProfilePicture = async () => {
+      return  await axios.get("https://randomuser.me/api").then((result)=>{return result.data.results[0].picture.large });
+        
+    };
+
+    const SignUp = async (email, username, password) => {
         try {
+            
             setLoading(true);
-            await createUserWithEmailAndPassword(authentication,email,username,password).then((result)=>{setLoading(false);}).catch((error)=>{ setLoading(false); alert("auth/email-already-in-use" in error ? "user with this email already exist": "please login user already exist")});
+            await createUserWithEmailAndPassword(authentication, email, password).then(async (result) => {
+                addDoc(collection(db, "users"), {
+                    ownerId: result.user.uid,
+                    username: username,
+                    email: result.user.email,
+                    profilePicture: await RandomProfilePicture(),
+                })
+            }).catch((error) => { setLoading(false); alert(error) }).finally((result)=>{setLoading(false);console.log(result)});
+            
 
         } catch (error) {
             setLoading(false);
@@ -25,10 +42,10 @@ export const RegisterComponent = ({ navigation }) => {
         }
     };
     return (
-        <Formik initialValues={{ email: "", password: "", username: "" }} onSubmit={(values) => { SignUp(values.email,values.username,values.password); }} validationSchema={RegisterFormSchema} validateOnMount={true} >
+        <Formik initialValues={{ email: "", password: "", username: "" }} onSubmit={(values) => { SignUp(values.email, values.username, values.password); }} validationSchema={RegisterFormSchema} validateOnMount={true} >
             {({ handleBlur, handleChange, handleSubmit, isValid, values, errors }) => (
                 <View style={styles.container}>
-                  {loading?(<ActivityIndicator size={"small"}/>):(<View><TextInput
+                    {loading ? (<ActivityIndicator size={"small"} />) : (<View><TextInput
                         style={[styles.textBox, { borderColor: values.email.length < 1 || validate(values.email) ? "black" : "red" }]}
                         placeholderTextColor={"grey"}
                         placeholder={"Phone number or email"}
@@ -41,33 +58,33 @@ export const RegisterComponent = ({ navigation }) => {
                         value={values.email}
 
                     />
-                    <TextInput
-                        style={[styles.textBox, { borderColor: values.username.length < 1 || values.username.length >= 6 ? "black" : "red" }]}
-                        placeholderTextColor={"grey"}
-                        placeholder={"username"}
-                        autoCapitalize="none"
-                        keyboardType="email-address"
-                        textContentType="emailAddress"
-                        autoFocus={true}
-                        onChangeText={handleChange("username")}
-                        onBlur={handleBlur("username")}
-                        value={values.username}
+                        <TextInput
+                            style={[styles.textBox, { borderColor: values.username.length < 1 || values.username.length >= 6 ? "black" : "red" }]}
+                            placeholderTextColor={"grey"}
+                            placeholder={"username"}
+                            autoCapitalize="none"
+                            keyboardType="email-address"
+                            textContentType="emailAddress"
+                            autoFocus={true}
+                            onChangeText={handleChange("username")}
+                            onBlur={handleBlur("username")}
+                            value={values.username}
 
-                    />
-                    {/* {errors.email} */}
-                    <Text></Text>
-                    <TextInput
-                        onChangeText={handleChange("password")}
-                        onBlur={handleBlur("password")}
-                        value={values.password}
-                        style={[styles.textBox, { borderColor: values.password.length < 1 || values.password.length >= 8 ? "black" : "red" }]} autoCapitalize="none" autoCorrect={false} placeholder="enter your password" textContentType="password" placeholderTextColor={"grey"} secureTextEntry={true} />
+                        />
+                        {/* {errors.email} */}
+                        <Text></Text>
+                        <TextInput
+                            onChangeText={handleChange("password")}
+                            onBlur={handleBlur("password")}
+                            value={values.password}
+                            style={[styles.textBox, { borderColor: values.password.length < 1 || values.password.length >= 8 ? "black" : "red" }]} autoCapitalize="none" autoCorrect={false} placeholder="enter your password" textContentType="password" placeholderTextColor={"grey"} secureTextEntry={true} />
 
-                    <View style={styles.button(isValid)}>
-                        <Button title="Sign up" onPress={handleSubmit} disabled={!isValid} color={"white"} />
-                    </View>
-                    <View style={{ flexDirection: "row", marginTop: 10, alignItems: "center", justifyContent: "center" }}>
-                        <Text>Already have an account ? </Text><TouchableOpacity onPress={() => { navigation.navigate("Login") }}><Text style={{ color: "#0096F6" }}>Sign in</Text></TouchableOpacity>
-                    </View></View>)}
+                        <View style={styles.button(isValid)}>
+                            <Button title="Sign up" onPress={handleSubmit} disabled={!isValid} color={"white"} />
+                        </View>
+                        <View style={{ flexDirection: "row", marginTop: 10, alignItems: "center", justifyContent: "center" }}>
+                            <Text>Already have an account ? </Text><TouchableOpacity onPress={() => { navigation.navigate("Login") }}><Text style={{ color: "#0096F6" }}>Sign in</Text></TouchableOpacity>
+                        </View></View>)}
                 </View>
             )}
         </Formik>
